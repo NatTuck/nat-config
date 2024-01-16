5b4a295c4f998fa9010121d1ca135ae24a6526b6
Nat Tuck <nat@ferrus.net>
2024-01-16 11:22:07 -0500

    initial

---
+++ setup.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use 5.30.0;
+use warnings FATAL => 'all';
+
+use File::Spec::Functions qw(splitpath);
+use JSON;
+
+my @confs = (
+    ['~/.config/emacs', 'git@github.com:NatTuck/emacs-config.git'],
+    ['~/.config/i3', 'git@github.com:NatTuck/i3-config.git'],
+);
+
+for my $pair (@confs) {
+    my ($path, $repo) = @$pair;
+    $path = expand($path);
+    next if (-d "$path");
+    system(qq{git clone "$repo" "$path"});
+}
+
+sub expand {
+    my ($xx) = @_;
+    my $home = $ENV{'HOME'};
+    $xx =~ s/\~/$home/g;
+    return $xx;
+}
