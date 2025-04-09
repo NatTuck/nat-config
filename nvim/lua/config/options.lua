cfa147faa465b6dea31303cc9c170171c8703664
Nat Tuck <nat@ferrus.net>
2025-04-09 12:01:00 -0400

    4 spaces for C

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,13 @@
 
 vim.g.minipairs_disable = true
 --vim.opt.relativenumber = false
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.bo.shiftwidth = 4
+    vim.bo.tabstop = 4
+    vim.bo.softtabstop = 4
+    vim.bo.expandtab = true
+  end,
+})
