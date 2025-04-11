1f26586058dbe9ad680f6f064dd865741595f363
Nat Tuck <nat@ferrus.net>
2025-04-10 21:28:41 -0400

    linewrap in markdown

---
+++ nvim/ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+-- Allow hard line wrapping with gqip/gqap.
+vim.opt_local.textwidth = 80
+vim.opt_local.formatoptions:append("t")
+vim.opt_local.formatexpr = "" -- Disable LSP formatting interference
