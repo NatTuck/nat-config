1298f0a89fa93ecf165a3bee8add40e1d9961678
Nat Tuck <nat@ferrus.net>
2025-04-23 13:17:19 -0400

    ruby to asdf

--- install-asdf.sh
+++ install-asdf.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-if [ ! -d ~/.asdf ]
-then
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+if [ ! -d ~/.asdf ]; then
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 fi
 
 . "$HOME/.asdf/asdf.sh"
@@ -20,9 +19,17 @@ asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf install erlang 26.2.5
 asdf global erlang 26.2.5
 
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf install elixir 1.16.2-otp-26
 asdf global elixir 1.16.2-otp-26
+
+apt-get install autoconf patch build-essential rustc libssl-dev \
+  libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev \
+  libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev libssl-dev
+
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf install ruby 3.4.3
+asdf global ruby 3.4.3
 
 echo "Add the following to ~/.bashrc:"
 echo
